9f4e5da891e3675b44def64425fec47f7781e62e
georgekazz <sfinas1998@hotmail.com>
2025-03-07 21:50:07 +0200

    add padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,10 +129,10 @@
                 </div>
 
                 <!-- Deloitte -->
-                <div class="p-4 bg-white rounded-lg shadow-md flex justify-center items-center">
+                <div class="p-6 bg-white rounded-lg shadow-md flex justify-center items-center">
                     <a href="https://www2.deloitte.com/" target="_blank" rel="noopener noreferrer">
                         <img src="{{ asset('/img/Deloitte.svg') }}" alt="Sponsor 2"
-                            class="w-40 hover:scale-105 transition duration-300 mx-auto">
+                            class="w-48 hover:scale-105 transition duration-300 mx-auto">
                     </a>
                 </div>
 
@@ -145,10 +145,10 @@
                 </div>
 
                 <!-- Lancom -->
-                <div class="p-4 bg-white rounded-lg shadow-md flex justify-center items-center">
+                <div class="p-6 bg-white rounded-lg shadow-md flex justify-center items-center">
                     <a href="https://www.lancom.gr" target="_blank" rel="noopener noreferrer">
                         <img src="{{ asset('/img/lancom-logo.svg') }}" alt="Sponsor 4"
-                            class="w-40 hover:scale-105 transition duration-300 mx-auto">
+                            class="w-48 hover:scale-105 transition duration-300 mx-auto">
                     </a>
                 </div>
             </div>
